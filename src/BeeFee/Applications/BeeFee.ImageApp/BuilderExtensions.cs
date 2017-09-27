172d63f41eaeebaa1a25af14927ff0e005cab95c
pasha2100 <brat11@mail.ru>
2017-09-27 21:14:43 +0500

    добавлен менеджер кеша+несколько тестов

--- src/BeeFee/Applications/BeeFee.ImageApp/BuilderExtensions.cs
+++ src/BeeFee/Applications/BeeFee.ImageApp/BuilderExtensions.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BeeFee.ImageApp.Caching;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace BeeFee.ImageApp
 {
-	public static class BuilderExtensions
-	{
-		public static IServiceCollection AddImageApp(this IServiceCollection service, string settingsJsonFile)
-			=> service.AddSingleton(x =>
-			{
-				var setting = x.GetService<ImagesAppStartSettings>();
-				return new ImageApp.Services.ImageService(setting.ImagesFolder, setting.PublicOriginalFolder,
-					setting.PrivateOriginalFolder, setting.ResizedFolder,
-					new ImageSize(setting.MaxOriginalWidth, setting.MaxOriginalHeight), settingsJsonFile,
-					setting.RemoveImageAvailabilityTime);
-			});
-	}
+    public static class BuilderExtensions
+    {
+        public static IServiceCollection AddImageApp(this IServiceCollection service, string settingsJsonFile)
+            => service.AddSingleton(x =>
+            {
+                var setting = x.GetService<ImagesAppStartSettings>();
+                return new ImageApp.Services.ImageService(setting.ImagesFolder, setting.PublicOriginalFolder,
+                    setting.PrivateOriginalFolder, setting.ResizedFolder,
+                    new ImageSize(setting.MaxOriginalWidth, setting.MaxOriginalHeight), settingsJsonFile,
+                    setting.RemoveImageAvailabilityTime);
+            }).AddSingleton(cfg => cfg.GetService<MemoryCacheManager>());
+    }
 }
